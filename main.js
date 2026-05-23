4882f4f0e555c7f0049490460116f728bc887c64
corvincor <vladslusarenko395@gmail.com>
2026-05-23 18:41:09 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,98 +1,89 @@
 import * as THREE from "three";
 
-import { GLTFLoader } from "https://unpkg.com/three@0.136.0/examples/jsm/loaders/GLTFLoader.js?module";
+import { GLTFLoader } from "https://unpkg.com/three@0.136.0/examples/jsm/loaders/GLTFLoader.js";
 
 import {
   CSS3DRenderer,
   CSS3DObject,
-} from "https://unpkg.com/three@0.136.0/examples/jsm/renderers/CSS3DRenderer.js?module";
+} from "https://unpkg.com/three@0.136.0/examples/jsm/renderers/CSS3DRenderer.js";
 
 let player;
 
 window.onYouTubeIframeAPIReady = () => {
-  player = new YT.Player(
-    "player",
-
-    {
-      videoId: "ywVRLuN6KWA",
-
-      playerVars: {
-        playsinline: 1,
-      },
+  player = new YT.Player("player", {
+    videoId: "YE7VzlLtp-4",
+    playerVars: {
+      playsinline: 1,
     },
-  );
+  });
 };
 
-window.addEventListener(
-  "DOMContentLoaded",
+window.addEventListener("DOMContentLoaded", async () => {
+  const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+    container: document.body,
+    imageTargetSrc: "./targets.mind",
+    maxTrack: 2,
+  });
 
-  async () => {
-    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-      container: document.body,
-      imageTargetSrc: "./targets.mind",
-      maxTrack: 2,
-    });
+  const { renderer, scene, camera } = mindarThree;
 
-    const { renderer, scene, camera } = mindarThree;
+  const cssRenderer = new CSS3DRenderer();
 
-    const cssRenderer = new CSS3DRenderer();
+  cssRenderer.setSize(window.innerWidth, window.innerHeight);
 
-    cssRenderer.setSize(window.innerWidth, window.innerHeight);
+  cssRenderer.domElement.style.position = "absolute";
 
-    cssRenderer.domElement.style.position = "absolute";
+  cssRenderer.domElement.style.top = "0";
 
-    cssRenderer.domElement.style.top = "0";
+  document.body.appendChild(cssRenderer.domElement);
 
-    document.body.appendChild(cssRenderer.domElement);
+  const cssScene = new THREE.Scene();
 
-    const cssScene = new THREE.Scene();
+  const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 
-    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+  scene.add(light);
 
-    scene.add(light);
+  const youtube = new CSS3DObject(document.getElementById("youtube"));
 
-    const youtube = new CSS3DObject(document.getElementById("youtube"));
+  youtube.scale.set(0.0015, 0.0015, 0.0015);
 
-    youtube.scale.set(0.0015, 0.0015, 0.0015);
+  const videoAnchor = mindarThree.addCSSAnchor(0);
 
-    const videoAnchor = mindarThree.addCSSAnchor(0);
+  videoAnchor.group.add(youtube);
 
-    videoAnchor.group.add(youtube);
+  videoAnchor.onTargetFound = () => {
+    document.getElementById("youtube").style.display = "block";
 
-    videoAnchor.onTargetFound = () => {
-      document.getElementById("youtube").style.display = "block";
+    if (player) {
+      player.playVideo();
+    }
+  };
 
-      if (player) {
-        player.playVideo();
-      }
-    };
+  videoAnchor.onTargetLost = () => {
+    if (player) {
+      player.pauseVideo();
+    }
+  };
 
-    videoAnchor.onTargetLost = () => {
-      if (player) {
-        player.pauseVideo();
-      }
-    };
+  const loader = new GLTFLoader();
 
-    const loader = new GLTFLoader();
+  const gltf = await loader.loadAsync(
+    "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@master/examples/image-tracking/assets/band-example/raccoon/scene.gltf",
+  );
 
-    const gltf = await loader.loadAsync(
-      "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@master/examples/image-tracking/assets/band-example/raccoon/scene.gltf",
-    );
+  gltf.scene.scale.set(0.1, 0.1, 0.1);
 
-    gltf.scene.scale.set(0.1, 0.1, 0.1);
+  gltf.scene.position.set(0, -0.4, 0);
 
-    gltf.scene.position.set(0, -0.4, 0);
+  const modelAnchor = mindarThree.addAnchor(1);
 
-    const modelAnchor = mindarThree.addAnchor(1);
+  modelAnchor.group.add(gltf.scene);
 
-    modelAnchor.group.add(gltf.scene);
+  await mindarThree.start();
 
-    await mindarThree.start();
+  renderer.setAnimationLoop(() => {
+    renderer.render(scene, camera);
 
-    renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
-
-      cssRenderer.render(cssScene, camera);
-    });
-  },
-);
+    cssRenderer.render(cssScene, camera);
+  });
+});
